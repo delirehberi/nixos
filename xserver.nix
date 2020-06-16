51d6e5c4861d8a458eba71f42d6c608321eb7d2b
Emre YILMAZ <emreyilmaz@yandex.com>
2020-06-17 00:05:18 +0300

    touchpad configurations, xlock time increased

--- xserver.nix
+++ xserver.nix
@@ -2,6 +2,25 @@
 
 {
   services.xserver = {
+    synaptics.dev=''/dev/input/event11'';
+    synaptics.enable = true; 
+    synaptics.additionalOptions = ''
+        Option "TapButton1" "1"
+        Option "TapButton2" "3"
+        Option "TapButton3" "2"
+        Option "VertEdgeScroll" "on"
+        Option "VertTwoFingerScroll" "on"
+        Option "HorizEdgeScroll" "on"
+        Option "HorizTwoFingerScroll" "on"
+        Option "CircularScrolling" "on"
+        Option "CircScrollTrigger" "2"
+        Option "EmulateTwoFingerMinZ" "40"
+        Option "EmulateTwoFingerMinW" "8"
+        Option "CoastingSpeed" "0"
+        Option "FingerLow" "30"
+        Option "FingerHigh" "50"
+        Option "MaxTapTime" "125"
+      '';
     enable = true;
     layout = "tr";
     autorun = true;
@@ -10,7 +29,7 @@
       locker = "${pkgs.i3lock-fancy}/bin/i3lock-fancy";
       enableNotifier = true;
       notifier = ''${pkgs.libnotify}/bin/notify-send "Locking in 10 seconds" '';
-      time = 5;
+      time = 30;
     };
     windowManager.i3.enable=true;
   };
